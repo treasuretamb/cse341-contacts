c2a507505a5a4e4cd7488a5384de9182545752a4
Treasure Tamb <treasuretamb@gmail.com>
2026-05-09 02:33:31 +0200

    Improve server startup and error handling

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+
 const mongodb = require('./db/connect');
 
 const app = express();
@@ -8,14 +9,15 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('CSE 341 Contacts API is running!');
 });
 
 app.use('/contacts', require('./routes/contacts'));
 
 mongodb.initDb((err) => {
   if (err) {
-    console.log(err);
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1);        // Exit if DB connection fails
   } else {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
